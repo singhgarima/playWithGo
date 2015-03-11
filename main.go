09b8c0e7d4ed7d6b5012be345b8a973cf1ed93bc
Garima Singh <igarimasingh@gmail.com>
2015-03-11 08:19:35 +0800

    Adding routes to main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello WOrld!")
+}
+
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/data/"):]
+    fmt.Fprintf(w, "Data ID: %s", id)
+}
+
+func main() {
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/data/", DataHandler)
+	http.ListenAndServe(":4321", nil)
+}
