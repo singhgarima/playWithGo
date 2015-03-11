5dc8eff4f804fbf732dafe77003df7a562fa2aba
Garima Singh <igarimasingh@gmail.com>
2015-03-11 21:27:29 +0800

    Adding test

--- webserver/src/main/main.go
+++ webserver/src/main/main.go
@@ -14,8 +14,18 @@ func DataHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Data ID: %s", id)
 }
 
+func GetOnly(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			h(w, r)
+			return
+		}
+		http.Error(w, "Only supports GET", http.StatusForbidden)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", HomeHandler)
-	http.HandleFunc("/data/{id:[0-9]+}", DataHandler)
+	http.HandleFunc("/", GetOnly(HomeHandler))
+	http.HandleFunc("/data/{id:[0-9]+}", GetOnly(DataHandler))
 	http.ListenAndServe(":4321", nil)
 }
---
+++ webserver/src/main/main_test.go
@@ -0,0 +1,28 @@
+package main_test
+
+import (
+	"testing"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+)
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Works")
+}
+
+type MyWriter http.ResponseWriter
+
+func TestGetOnly(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	
+	url := "http://foo-bar-blahblah.com"	
+	request, _ := http.NewRequest("GET", url, nil)
+	
+	GetOnly(TestHandler)(recorder, request)
+	
+	if(recorder.Body.String() != "Works") {
+		t.Error("should allow get, is failing")
+	}
+	
+}
