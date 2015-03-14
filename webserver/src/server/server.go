7f902f295bc867948391d5b110533b5fb5b44ff5
Garima Singh <igarimasingh@gmail.com>
2015-03-14 12:06:07 +0800

    Adding search logic in server

--- webserver/src/server/server.go
+++ webserver/src/server/server.go
@@ -3,15 +3,28 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"lru_cache"
+	"strconv"
 )
+
+var cache lru_cache.CacheStore
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello WOrld!")
 }
 
 func DataHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Path[len("/data/"):]
-    fmt.Fprintf(w, "Data ID: %s", id)
+	id, conversionErr := strconv.Atoi(r.URL.Path[len("/data/"):])
+	if ( conversionErr == nil ) {
+		data, searchErr := cache.GetValueForKey(id)
+		if ( searchErr == nil ) {
+	    	fmt.Fprintf(w, "Data for ID: %d is %s", id, data)
+		} else {
+			http.NotFound(w, r)
+		}
+	} else {
+		http.NotFound(w, r)
+	}
 }
 
 func GetOnly(h http.HandlerFunc) http.HandlerFunc {
@@ -25,6 +38,9 @@ func GetOnly(h http.HandlerFunc) http.HandlerFunc {
 }
 
 func main() {
+	cache = lru_cache.CreateCache(10)
+	cache.StoreValueForKey("some value", 1)
+	
 	http.HandleFunc("/", GetOnly(HomeHandler))
 	http.HandleFunc("/data/", GetOnly(DataHandler))
 	http.ListenAndServe(":4321", nil)
--- webserver/src/server/server_test.go
+++ webserver/src/server/server_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"lru_cache"
 )
 
 func DummyHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,28 +13,66 @@ func DummyHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func TestGetOnly(t *testing.T) {
+	//setup
 	recorder := httptest.NewRecorder()
-
 	url := "http://foo-bar-blahblah.com"
 	request, _ := http.NewRequest("GET", url, nil)
 
+	//test
 	GetOnly(DummyHandler)(recorder, request)
 
+	//assertions
 	if(recorder.Body.String() != "Works") {
 		t.Error("should allow get, is failing")
 	}
 
 }
 
-func TestDataHandler(t *testing.T) {
+func TestDataHandlerSuccess(t *testing.T) {
+	//setup
+	cache = lru_cache.CreateCache(10)
+	cache.StoreValueForKey("some value", 1)
+	
 	recorder := httptest.NewRecorder()
-
 	url := "http://foo-bar-blahblah.com/data/1"
 	request, _ := http.NewRequest("GET", url, nil)
 
+	//test
 	DataHandler(recorder, request)
 
-	if(recorder.Body.String() != "Data ID: 1") {
-		t.Error("should allow get, is failing")
+	//assertions
+	if(recorder.Body.String() != "Data for ID: 1 is some value") {
+		t.Error("Incorrect Data received")
 	}
 }
+
+func TestDataHandlerDailure(t *testing.T) {
+	//setup
+	cache = lru_cache.CreateCache(10)
+	cache.StoreValueForKey("some value", 1)
+	
+	recorder := httptest.NewRecorder()
+	url := "http://foo-bar-blahblah.com/data/2"
+	request, _ := http.NewRequest("GET", url, nil)
+
+	//test
+	DataHandler(recorder, request)
+
+	//assertions
+	if(recorder.Body.String() != "404 page not found\n") {
+		t.Error("Expecting Body as 404 page not found")
+	}
+	if(recorder.Code != http.StatusNotFound) {
+		t.Error("Incorrect Status Code expecting 404")
+	}
+}
+
+// func TestMain(t *testing.T) {
+// 	var cache2 lru_cache.CacheStore
+// 	cache := lru_cache.CreateCache(10)
+// 	fmt.Println("Main")
+// 	fmt.Println(reflect.TypeOf(cache))
+// 	fmt.Println(reflect.TypeOf(cache2))
+
+// 	t.Error("should allow get, is failing")
+// }
